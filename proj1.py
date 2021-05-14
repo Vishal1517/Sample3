e116aff7f71144f476472d41864e2b479ad0333e
Vishal Farakate <vishal1517@gmail.com>
2021-05-14 22:34:43 +0530

    added power function

--- proj1.py
+++ proj1.py
@@ -12,12 +12,15 @@ def multiply(a, b):
 def devide(a, b):
     print(a/b)
 
+def power(a, b):
+    print(a**b)
 
 # function call
-no1 = 5
-no2 = 15
+no1 = 10
+no2 = 2
 
 sum(no1, no2)
 substract(no1, no2)
 multiply(no1, no2)
 devide(no1, no2)
+power(no1, no2)
