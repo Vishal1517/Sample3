fb91d4915362409160e35eb228d9d5a5e30ff1d4
Vishal Farakate <vishal1517@gmail.com>
2021-05-14 22:46:47 +0530

    naming

--- proj1.py
+++ proj1.py
@@ -1,16 +1,17 @@
-print("Hello World")
+print("Standard Calculator")
 
 def sum(a, b):
-	print(a+b)
+	print("sum -->", a+b)
 
 def substract(a, b):
-    print(a-b)
+    print("substract -->", a-b)
     
 def multiply(a, b):
-    print(a*b)
+    print("multiply -->", a*b)
 
 def devide(a, b):
-    print(a/b)
+    print("devide -->", a/b)
+
 
 
 # function call
@@ -21,3 +22,6 @@ sum(no1, no2)
 substract(no1, no2)
 multiply(no1, no2)
 devide(no1, no2)
+
+
+print("Advance Calculator")
